2017cbe2a185296ebb20cc7d38ffdfb04adbd261
rind-tran <53333438+rind-tran@users.noreply.github.com>
2024-02-28 22:46:50 +0700

    Create question1.py

---
+++ question1.py
@@ -0,0 +1,18 @@
+from mrjob.job import MRJob
+
+# This includes our definition of map and reduce functions
+class MyMapReduce(MRJob):
+
+    # The key is vendorID and the value is the total amount charged to passengers.
+    def mapper(self, _, line):
+        line = line.split(",")
+        if (line[0] != "VendorID"):
+            yield (line[0], float(line[16]))
+
+    # Our reducer takes a group of (key, value) where key = word, and produces a final (key, value)
+    # The key is a word and its value is the number of occurrences of the word
+    def reducer(self, vendorID, total_amount):
+        yield (vendorID, sum(total_amount))
+
+if __name__ == '__main__':
+    MyMapReduce.run()
