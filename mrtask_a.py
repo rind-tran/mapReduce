dde1beab85db32fb8e7804562b45c6c1f0bd4510
rind-tran <53333438+rind-tran@users.noreply.github.com>
2024-03-03 16:08:33 +0700

    Update mrtask_a.py

--- mrtask_a.py
+++ mrtask_a.py
@@ -3,15 +3,20 @@ from mrjob.job import MRJob
 # This includes our definition of map and reduce functions
 class MyMapReduce(MRJob):
 
-    # The key is vendorID and the value is the total amount charged to passengers.
+    # The key is vendorID and the value is (1: count ad 1 trip and the total amount charged to passengers.)
     def mapper(self, _, line):
         line = line.split(",")
         if (line[0] != "VendorID"):
-            yield (line[0], float(line[16]))
+            yield line[0], (1, float(line[16]))
 
     # Our reducer takes a group of (key, value) where key = word, and produces a final (key, value)
     def reducer(self, vendorID, total_amount):
-        yield (vendorID, sum(total_amount))
+        counts = 0
+        total = 0
+        for count, revenue in total_amount:
+            counts += count
+            total += revenue
+        yield vendorID, (counts, round(total, 2))
 
 if __name__ == '__main__':
     MyMapReduce.run()
